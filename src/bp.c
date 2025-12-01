815e3e534c870b137c25555a086b6d017932ca12
aregmk6 <areg631@gmail.com>
2025-12-01 18:11:56 +0200

    finish first try, without debug

--- src/bp.c
+++ src/bp.c
@@ -14,7 +14,7 @@ typedef uint32_t tag_t;
 typedef uint32_t addr_t;
 typedef uint8_t histbuf_t;
 
-typedef enum { SNT, WNT, WT, ST } predictor_t;
+typedef enum { MIN_BOUND, SNT, WNT, WT, ST, MAX_BOUND } predictor_t;
 
 typedef struct {
     tag_t m_tag;
@@ -50,39 +50,13 @@ typedef struct {
 static BTB btb;
 
 short my_log2(uint8_t size);
-
-size_t calc_mem_usage() {
-    uint8_t n_entries = btb.m_size;
-    uint8_t tag_size = btb.m_tag_size;
-    uint8_t target_size = my_log2(btb.m_size);
-    uint8_t history_size = btb.m_history_size;
-    uint8_t fsm_tbl_size = B_FSM_SIZE * (2 ^ history_size);
-
-    size_t mem_size;
-    if (!btb.is_global_history && !btb.is_global_table) {
-        mem_size = n_entries         //
-                   * (tag_size       //
-                      + target_size  //
-                      + history_size //
-                      + fsm_tbl_size);
-    } else if (btb.is_global_history && !btb.is_global_table) {
-        mem_size = n_entries              //
-                       * (tag_size        //
-                          + target_size   //
-                          + fsm_tbl_size) //
-                   + history_size;
-    } else if (!btb.is_global_history && btb.is_global_table) {
-        mem_size = n_entries              //
-                       * (tag_size        //
-                          + target_size   //
-                          + history_size) //
-                   + fsm_tbl_size;
-    } else {
-        mem_size = n_entries * (tag_size + target_size) //
-                   + (history_size + fsm_tbl_size);
-    }
-    return mem_size;
-}
+size_t calc_mem_usage();
+short get_entry(uint32_t pc);
+tag_t get_tag(uint32_t pc);
+uint8_t get_fsm_arr_idx(const histbuf_t hist_buf, const BTB_entry entery);
+void update_hist_buf(histbuf_t *hist_buf, bool taken);
+uint8_t get_history(const histbuf_t hist_buf);
+short ttp(uint8_t exponent); // ttp = two to the power
 
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
             unsigned fsmState, bool isGlobalHist, bool isGlobalTable,
@@ -113,7 +87,7 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
         btb.m_history = 0;
     }
 
-    const int fsm_arr_size = 2 ^ btb.m_history_size;
+    const int fsm_arr_size = ttp(btb.m_history_size);
     if (!btb.is_global_table) {
         for (int ent_idx = 0; ent_idx < btb.m_size; ent_idx++) {
             predictor_t *entr_fsm_arr = btb.m_ent[ent_idx].m_local_fsm_arr;
@@ -132,44 +106,6 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize,
     }
 
     return 0; // success
-}
-
-short my_log2(uint8_t size) {
-    short result = 0;
-    while (size >>= 1)
-        result++;
-    return result;
-}
-
-short get_entry(uint32_t pc) {
-    const int b_idx_len = my_log2(btb.m_size);
-    uint32_t ent = pc >> B_MEM_ALIGN_LEN;
-    ent <<= B_ADDR_LEN - b_idx_len;
-    ent >>= B_ADDR_LEN - b_idx_len;
-    return ent;
-}
-
-tag_t get_tag(uint32_t pc) {
-    const int b_idx_len = my_log2(btb.m_size);
-    tag_t tag = pc >> (b_idx_len + B_MEM_ALIGN_LEN);
-    tag <<= B_ADDR_LEN - btb.m_tag_size;
-    tag >>= B_ADDR_LEN - btb.m_tag_size;
-    return tag;
-}
-
-uint8_t get_fsm_arr_idx(const histbuf_t hist_buf) {
-    if (btb.m_history_size == B_HIST_MAX_SIZE)
-        return hist_buf; // cause hist_buf is a uint8_t anyway
-    histbuf_t idx = hist_buf << (B_HIST_MAX_SIZE - btb.m_history_size);
-    idx >>= B_HIST_MAX_SIZE - btb.m_history_size;
-    return idx;
-}
-
-void update_hist_buf(histbuf_t *hist_buf, bool taken) {
-    *hist_buf <<= 1;
-    if (!taken)
-        return;
-    *hist_buf |= 1; // 1 is a mask
 }
 
 // true for taken - false for not taken
@@ -194,25 +130,156 @@ bool BP_predict(uint32_t pc, uint32_t *dst) {
     else
         cur_pred_arr = btb.m_fsm_arr;
 
-    uint8_t arr_idx = get_fsm_arr_idx(*cur_hist_buf);
+    uint8_t arr_idx = get_fsm_arr_idx(*cur_hist_buf, *cur_ent);
 
     switch (cur_pred_arr[arr_idx]) {
     case ST:
     case WT:
+        *dst = cur_ent->m_target_addr;
         return true;
     case WNT:
     case SNT:
+        *dst += 4;
         return false;
+    default:
+        return false; // this shouldn't happen
     }
 }
 
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst) {
+    short ent = get_entry(pc);
+    tag_t tag = get_tag(pc);
+    BTB_entry *cur_ent = &btb.m_ent[ent];
 
-    return;
+    cur_ent->m_tag = tag;
+    cur_ent->m_target_addr = targetPc;
+
+    histbuf_t *cur_hist_buf = NULL;
+    predictor_t *cur_pred_arr = NULL;
+
+    if (!btb.is_global_history)
+        cur_hist_buf = &cur_ent->m_local_history;
+    else
+        cur_hist_buf = &btb.m_history;
+    if (!btb.is_global_table)
+        cur_pred_arr = cur_ent->m_local_fsm_arr;
+    else
+        cur_pred_arr = btb.m_fsm_arr;
+    uint8_t arr_idx = get_fsm_arr_idx(*cur_hist_buf, *cur_ent);
+
+    bool prediction = BP_predict(pc, &pred_dst);
+    if (prediction != taken) {
+        btb.m_flush_num++;
+    }
+    btb.m_br_num++;
+
+    update_hist_buf(cur_hist_buf, taken);
+    predictor_t next_state;
+    if (taken == true) {
+        next_state = (predictor_t)((int)cur_pred_arr[arr_idx] + 1);
+        if (next_state == MAX_BOUND)
+            next_state = ST;
+    } else {
+        next_state = (predictor_t)((int)cur_pred_arr[arr_idx] - 1);
+        if (next_state == MIN_BOUND)
+            next_state = SNT;
+    }
+    cur_pred_arr[arr_idx] = next_state;
 }
 
 void BP_GetStats(SIM_stats *curStats) {
     curStats->flush_num = btb.m_flush_num;
     curStats->br_num = btb.m_br_num;
     curStats->size = btb.m_b_mem_size;
+}
+
+// -------------------- helper functions --------------------
+
+short my_log2(uint8_t size) {
+    short result = 0;
+    while (size >>= 1)
+        result++;
+    return result;
+}
+
+short ttp(uint8_t exponent) { return (2 << exponent); }
+
+short get_entry(uint32_t pc) {
+    const int b_idx_len = my_log2(btb.m_size);
+    uint32_t ent = pc >> B_MEM_ALIGN_LEN;
+    ent <<= B_ADDR_LEN - b_idx_len;
+    ent >>= B_ADDR_LEN - b_idx_len;
+    return ent;
+}
+
+tag_t get_tag(uint32_t pc) {
+    const int b_idx_len = my_log2(btb.m_size);
+    tag_t tag = pc >> (b_idx_len + B_MEM_ALIGN_LEN);
+    tag <<= B_ADDR_LEN - btb.m_tag_size;
+    tag >>= B_ADDR_LEN - btb.m_tag_size;
+    return tag;
+}
+
+uint8_t get_history(const histbuf_t hist_buf) {
+    if (btb.m_history_size == B_HIST_MAX_SIZE)
+        return hist_buf; // cause hist_buf is a uint8_t anyway
+
+    histbuf_t history = hist_buf << (B_HIST_MAX_SIZE - btb.m_history_size);
+    history >>= B_HIST_MAX_SIZE - btb.m_history_size;
+    return history;
+}
+
+uint8_t get_fsm_arr_idx(const histbuf_t hist_buf, const BTB_entry entery) {
+    uint8_t arr_idx;
+    if (btb.share) {
+        arr_idx = get_history(hist_buf) ^
+                  entery.IP_addr; // probably need to change this
+    }
+
+    arr_idx = get_history(hist_buf);
+    return arr_idx;
+}
+
+void update_hist_buf(histbuf_t *hist_buf, bool taken) {
+    *hist_buf <<= 1;
+    if (!taken)
+        return;
+    *hist_buf |= 1; // 1 is a mask
+}
+
+size_t calc_mem_usage() {
+    uint8_t n_entries = btb.m_size;
+    uint8_t tag_size = btb.m_tag_size;
+    uint8_t target_size = my_log2(btb.m_size);
+    uint8_t history_size = btb.m_history_size;
+    uint8_t fsm_tbl_size = B_FSM_SIZE * (ttp(history_size));
+
+    size_t mem_size;
+    if (!btb.is_global_history && !btb.is_global_table) {
+        mem_size = n_entries         //
+                   * (tag_size       //
+                      + target_size  //
+                      + history_size //
+                      + fsm_tbl_size);
+
+    } else if (btb.is_global_history && !btb.is_global_table) {
+        mem_size = n_entries              //
+                       * (tag_size        //
+                          + target_size   //
+                          + fsm_tbl_size) //
+                   + history_size;
+
+    } else if (!btb.is_global_history && btb.is_global_table) {
+        mem_size = n_entries              //
+                       * (tag_size        //
+                          + target_size   //
+                          + history_size) //
+                   + fsm_tbl_size;
+
+    } else {
+        mem_size = n_entries * (tag_size + target_size) //
+                   + (history_size + fsm_tbl_size);
+    }
+
+    return mem_size;
 }
